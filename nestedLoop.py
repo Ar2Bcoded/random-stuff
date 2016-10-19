ca553991adcbc6e7094b0e41bbba505aeccede64
Ar2Bcoded <dadasw2@interia.pl>
2016-10-19 20:28:01 +0200

    Create nestedLoop.py

---
+++ nestedLoop.py
@@ -0,0 +1,18 @@
+# character picture grid
+# nested loop example
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+colList = []
+
+for i in range(len(grid[0])):
+        for y in range(len(grid)):
+               print(grid[y][i], end='')
+        print('\n')
